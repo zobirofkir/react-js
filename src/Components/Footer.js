bc4ff2c795a1d8a39c866c68b4ca90657d32da41
zobirofkir <zobirofkir19@gmail.com>
2024-06-23 14:09:05 +0100

    hhh

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
       <footer class="bg-black rounded-lg shadow m-4">
           <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center text-white">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+            <span class="text-sm text-gray-500 sm:text-center text-white">© 3034 <a href="https://flowbite.com/" class="hover:underline">ZOBIR OFKIR</a>. All Rights Reserved.
           </span>
           <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
               <li>
