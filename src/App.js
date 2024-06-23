5ec28ab75d3cef952488215a027c5de145361b5b
zobirofkir <zobirofkir19@gmail.com>
2024-06-23 16:14:13 +0100

    nkaejnfkljz

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Body from "./Components/Body"
 import Footer from "./Components/Footer"
 function App() {
   return (
-    <>
+    <div>
       <Header/>
       <Body/>
       <About/>
       <Contact/>
       <Footer/>
-    </>
+    </div>
   )
 }
 
